8f241e9479fcb5723c03060da1887f5239c4b186
DrugsZ <1002881508@qq.com>
2018-10-16 21:29:39 +0800

    resver

--- 15.3sum.js
+++ 15.3sum.js
@@ -1,30 +1,6 @@
 /*
  * [15] 3Sum
  *
-<<<<<<< HEAD
- * https://leetcode.com/problems/3sum/description/
- *
- * algorithms
- * Medium (22.15%)
- * Total Accepted:    395.8K
- * Total Submissions: 1.8M
- * Testcase Example:  '[-1,0,1,2,-1,-4]'
- *
- * Given an array nums of n integers, are there elements a, b, c in nums such
- * that a + b + c = 0? Find all unique triplets in the array which gives the
- * sum of zero.
- * 
- * Note:
- * 
- * The solution set must not contain duplicate triplets.
- * 
- * Example:
- * 
- * 
- * Given array nums = [-1, 0, 1, 2, -1, -4],
- * 
- * A solution set is:
-=======
  * https://leetcode-cn.com/problems/3sum/description/
  *
  * algorithms
@@ -41,7 +17,6 @@
  * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
  * 
  * 满足要求的三元组集合为：
->>>>>>> f14d20312f9325c6e0b56df379c9bed3b195af5f
  * [
  * ⁠ [-1, 0, 1],
  * ⁠ [-1, -1, 2]
@@ -54,33 +29,6 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-<<<<<<< HEAD
-    nums = nums.sort((a,b)=>a-b)
-    const result = []
-
-    for (let i = 0; i < nums.length-2; i++) {
-        let j = i+1, k = nums.length-1
-        const resver = -nums[i]
-        console.log(i)
-
-        // while (j < k) {
-        //     if (nums[j] + nums[k] > resver) {
-        //         k--
-        //     }else if (nums[j] + nums[k] < resver) {
-        //         j++
-        //     }else{
-        //         result.push(nums[j])
-        //         result.push(nums[i])
-        //         result.push(nums[k])
-        //     }
-        // }
-    }
-
-    return nums
-};
-
-console.log(threeSum([-1,-2,3,4]));
-=======
   nums.sort((a,b)=>a-b);
   const results = []
 
@@ -117,4 +65,3 @@ console.log(threeSum([-1,-2,3,4]));
 
   return results
 };
->>>>>>> f14d20312f9325c6e0b56df379c9bed3b195af5f
