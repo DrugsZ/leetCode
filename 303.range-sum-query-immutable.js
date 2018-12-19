8cc314cb7d288bd22025ace619e9dd146afa704d
DrugsZ <1002881508@qq.com>
2018-12-19 16:24:45 +0800

    [303] 区域和检索 - 数组不可变

---
+++ 303.range-sum-query-immutable.js
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=javascript
+ *
+ * [303] 区域和检索 - 数组不可变
+ *
+ * https://leetcode-cn.com/problems/range-sum-query-immutable/description/
+ *
+ * algorithms
+ * Easy (48.21%)
+ * Total Accepted:    5.3K
+ * Total Submissions: 11K
+ * Testcase Example:  '["NumArray","sumRange","sumRange","sumRange"]\n[[[-2,0,3,-5,2,-1]],[0,2],[2,5],[0,5]]'
+ *
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ * 
+ * 示例：
+ * 
+ * 给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+ * 
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 
+ * 说明:
+ * 
+ * 
+ * 你可以假设数组不可变。
+ * 会多次调用 sumRange 方法。
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.nums = [nums.length + 1];
+    this.nums[0] = 0
+    let num = 0;
+    nums.forEach((item,index) => {
+      num += item;
+      this.nums[index + 1] = num;
+    })
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    return this.nums[j+1] - this.nums[i]
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = Object.create(NumArray).createNew(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
