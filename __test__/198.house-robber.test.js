756638a1e946fcaf97379c34f18c750baa8b45bc
DrugsZ <1002881508@qq.com>
2018-12-20 10:20:49 +0800

    update unit test

--- __test__/198.house-robber.test.js
+++ __test__/198.house-robber.test.js
@@ -3,3 +3,10 @@ const rob = require('../198.house-robber.js')
 test('test to expect 4', () => {
   expect(rob([2,1,1,2])).toBe(4);
 })
+
+test('test to expect 5', () => {
+  expect(rob([1,2,4,2])).toBe(5);
+})
+test('test to expect 5 not 4', () => {
+  expect(rob([1,2,4,2])).not.toBe(4);
+})
