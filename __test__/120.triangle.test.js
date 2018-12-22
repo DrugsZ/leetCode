f828663fd4f97dbb9e1b27c00975e35b24aabdd8
DrugsZ <1002881508@qq.com>
2018-12-22 22:42:53 +0800

    添加部分测试用例

--- __test__/120.triangle.test.js
+++ __test__/120.triangle.test.js
@@ -1,10 +1,19 @@
 const minimumTotal = require('../120.triangle')
 
-test('三角形最小路径和',()=>{
+test('三角形最小路径和 => `11`',()=>{
     expect(minimumTotal([
         [2],
        [3,4],
       [6,5,7],
      [4,1,8,3]
     ])).toBe(11)
+})
+test('三角形最小路径和 => `0`',()=>{
+    expect(minimumTotal([])).toBe(0)
+})
+test('三角形最小路径和 => `2`',()=>{
+    expect(minimumTotal([
+        [1],
+       [1,2]
+    ])).toBe(2)
 })
