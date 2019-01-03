f18a9ba9eb5a98551a3e9c976b4753e2b1ac77b0
DrugsZ <1002881508@qq.com>
2019-01-03 15:18:47 +0800

    [21] 合并两个有序链表测试用例

---
+++ __test__/21.merge-two-sorted-lists.test.js
@@ -0,0 +1,20 @@
+const { mergeTwoLists } = require('../21.merge-two-sorted-lists');
+const { arr2List } = require('../util');
+
+test('should expect [1,1,2,3,4,5]', () => {
+  let l1 = arr2List([1,2,4]);
+  let l2 = arr2List([1,3,5]);
+  expect(mergeTwoLists(l1,l2)).toEqual(arr2List([1,1,2,3,4,5]));
+});
+
+test('test no l2', () => {
+  let l1 = arr2List([1,2,4]);
+  let l2 = null;
+  expect(mergeTwoLists(l1,l2)).toEqual(l1);
+});
+
+test('test no l1', () => {
+  let l1 = null;
+  let l2 = arr2List([1,3,5]);
+  expect(mergeTwoLists(l1,l2)).toEqual(l2);
+});
