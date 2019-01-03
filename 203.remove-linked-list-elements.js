33f1c0caee11934935e00d7e42b94d8cbc9755a1
DrugsZ <1002881508@qq.com>
2019-01-03 16:23:36 +0800

    feat(203.remove-linked-list-elements): [203] 移除链表元素
    删除链表中等于给定值 val 的所有节点。

---
+++ 203.remove-linked-list-elements.js
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=javascript
+ *
+ * [203] 移除链表元素
+ *
+ * https://leetcode-cn.com/problems/remove-linked-list-elements/description/
+ *
+ * algorithms
+ * Easy (37.67%)
+ * Total Accepted:    12.8K
+ * Total Submissions: 34K
+ * Testcase Example:  '[1,2,6,3,4,5,6]\n6'
+ *
+ * 删除链表中等于给定值 val 的所有节点。
+ * 
+ * 示例:
+ * 
+ * 输入: 1->2->6->3->4->5->6, val = 6
+ * 输出: 1->2->3->4->5
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+  if(!head)return head;
+  let start = head;
+
+  while ( start && start.next) {
+    if (start.next.val === val) {
+      start.next = start.next.next;
+    } else {
+      start = start.next
+    }
+  }
+  return val === head.val ? head.next : head;
+};
+
+module.exports = {
+  removeElements
+}
