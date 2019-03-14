7f2b1db6746b3ce5a33186daf709ed2321b51a07
DrugsZ <1002881508@qq.com>
2019-03-14 10:57:55 +0800

    feat(util): 增加`arrToTree`函数
    将数组转化为二叉树

--- util/index.js
+++ util/index.js
@@ -23,6 +23,48 @@ function arr2List(arr){
   return head;
 }
 
+/**
+ * 
+ * @param {Number} val 节点值
+ * @return {TreeNode}
+ */
+function CreateTreeNode (val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+/**
+ * 
+ * @param {Array.<Number>} arr 将要转化为二叉树的数组
+ */
+let arrToTree = (arr) => {
+  if (!arr.length) return null;
+  let i = 0;
+  let root = new CreateTreeNode(arr[i++]);
+  let nodeQueue = [root];
+  let curNode;
+  let len = arr.length;
+
+  while (nodeQueue.length) {
+    curNode = nodeQueue.shift();
+    if(i < len){
+      curNode.left = new CreateTreeNode(arr[i++]);
+      nodeQueue.push(curNode.left);
+    }else{
+      break;
+    }
+
+    if(i < len) {
+      curNode.right = new CreateTreeNode(arr[i++]);
+      nodeQueue.push(curNode.right);
+    }else{
+      break;
+    }
+  }
+
+  return root;
+}
+
 module.exports = {
-  arr2List
+  arr2List,
+  arrToTree
 }
