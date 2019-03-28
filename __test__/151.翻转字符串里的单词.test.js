db44ca6cd7ea5617642471beb5d0a1fa321cf8ab
DrugsZ <1002881508@qq.com>
2019-03-28 09:27:15 +0800

    fix:修复导出错误

--- __test__/151.翻转字符串里的单词.test.js
+++ __test__/151.翻转字符串里的单词.test.js
@@ -1,4 +1,4 @@
-const reverseWords = require('../151.翻转字符串里的单词');
+const { reverseWords } = require('../151.翻转字符串里的单词');
 
 test('反转字符串中的单词', () => {
   expect(reverseWords('the sky is blue')).toBe('blue is sky the');
