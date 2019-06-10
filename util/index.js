e355aa5ba08cc741fcae7dfc82e21ea1ea504fdb
DrugsZ <1002881508@qq.com>
2019-06-10 23:30:21 +0800

    style(util): 调整代码

--- util/index.js
+++ util/index.js
@@ -5,66 +5,66 @@ function ListNode(val) {
 
 /**
  * 根据数组创建链表
- * @param {Array<number>} arr 
+ * @param {Array<number>} arr
  */
-function arr2List(arr){
+function arr2List(arr) {
   let node;
   let head;
-  arr.forEach(item => {
-    let temp = new ListNode(item)
-    if(node){
-      node.next = temp
-    }else{
-      head = temp
+  arr.forEach((item) => {
+    const temp = new ListNode(item);
+    if (node) {
+      node.next = temp;
+    } else {
+      head = temp;
     }
-    node = temp
-  })
+    node = temp;
+  });
 
   return head;
 }
 
 /**
- * 
+ *
  * @param {Number} val 节点值
  * @return {TreeNode}
  */
-function CreateTreeNode (val) {
+function CreateTreeNode(val) {
   this.val = val;
   this.left = this.right = null;
 }
 /**
- * 
+ *
  * @param {Array.<Number>} arr 将要转化为二叉树的数组
  */
-let arrToTree = (arr) => {
+const arrToTree = (arr) => {
   if (!arr.length) return null;
   let i = 0;
-  let root = new CreateTreeNode(arr[i++]);
-  let nodeQueue = [root];
+  const root = new CreateTreeNode(arr[i++]);
+  const nodeQueue = [root];
   let curNode;
-  let len = arr.length;
+  const len = arr.length;
 
   while (nodeQueue.length) {
     curNode = nodeQueue.shift();
-    if(i < len){
+    if (i < len) {
       curNode.left = new CreateTreeNode(arr[i++]);
       nodeQueue.push(curNode.left);
-    }else{
+    } else {
       break;
     }
 
-    if(i < len) {
+    if (i < len) {
       curNode.right = new CreateTreeNode(arr[i++]);
       nodeQueue.push(curNode.right);
-    }else{
+    } else {
       break;
     }
   }
 
   return root;
-}
+};
 
 module.exports = {
   arr2List,
-  arrToTree
-}
+  arrToTree,
+};
