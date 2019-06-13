3d12edc68fbeb427b5dd063c9c6c1b0c4d04ce75
DrugsZ <1002881508@qq.com>
2019-06-13 22:58:54 +0800

    feat: 448.找出所有数组中消失的数字

---
+++ __test__/448.找出所有数组中消失的数字.test.js
@@ -0,0 +1,7 @@
+const { findDisappearedNumbers } = require('../448.找到所有数组中消失的数字');
+
+describe('找出所有数组中消失的数字', () => {
+  test('测试正确数组', () => {
+    expect(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])).toEqual([5, 6]);
+  });
+});
