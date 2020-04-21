349fe82731ede260f620b8e1327044afc211e399
DrugsZ <1002881508@qq.com>
2020-04-21 22:52:23 +0800

    feat:24.两辆交换链表中的节点

---
+++ 24.两两交换链表中的节点.js
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=javascript
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const swapPairs = (head) => {
+  if (!head || !head.next) {
+    return head;
+  }
+  let pre;
+  let current = head;
+  let currentNext = head.next;
+  const cacheHead = currentNext;
+  while (current && currentNext) {
+    const temp = currentNext.next;
+    currentNext.next = current;
+    current.next = temp;
+    if (pre) {
+      pre.next = currentNext;
+    }
+    pre = current;
+    current = temp;
+    if (current) {
+      currentNext = current.next;
+    } else {
+      currentNext = current;
+    }
+  }
+  return cacheHead;
+};
+// @lc code=end
